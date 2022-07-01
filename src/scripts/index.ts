8915f69bb37f03ea56b4bc53cbec53588f0c7109
Kroustille <6004961+Kroustille@users.noreply.github.com>
2022-07-01 21:37:15 +0200

    tech: add possibilities count display

--- src/scripts/index.ts
+++ src/scripts/index.ts
@@ -1,3 +1,4 @@
+import { getPossibilitiesCount } from './possibilities'
 import { resetPickedValues } from './reset'
 
 document.addEventListener('DOMContentLoaded', () => {
@@ -7,4 +8,10 @@ document.addEventListener('DOMContentLoaded', () => {
   }
 
   reset_button.addEventListener('click', resetPickedValues)
+
+  const possibilities_count_element = document.getElementById('possibilities_count')
+  if (possibilities_count_element) {
+    const possibilities_count = getPossibilitiesCount()
+    possibilities_count_element.textContent = `${possibilities_count}`
+  }
 })
