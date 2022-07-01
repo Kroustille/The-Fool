682f4ef6587bdf31e0b8b8f4d29404415cdefe7e
Kroustille <6004961+Kroustille@users.noreply.github.com>
2022-07-01 20:19:00 +0200

    feat: add initial value refresh

--- src/scripts/index.ts
+++ src/scripts/index.ts
@@ -1,1 +1,51 @@
-console.log('Hello world')
+document.addEventListener('DOMContentLoaded', () => {
+  const reset_button = document.getElementById('reset')
+  if (!reset_button) {
+    throw new Error('reset button not found')
+  }
+
+  reset_button.addEventListener('click', resetPickedValues)
+})
+
+const resetPickedValues = () => {
+  const { actor, action, details } = getValueElements()
+
+  actor.textContent = pickActor()
+  action.textContent = pickAction()
+  details.textContent = pickDetails()
+}
+
+const pickActor = () => {
+  return 'Pirate'
+}
+
+const pickAction = () => {
+  return 'Creuser'
+}
+
+const pickDetails = () => {
+  return 'Saucisson'
+}
+
+const getValueElements = () => {
+  const actor_element = document.getElementById('actor')
+  if (!actor_element) {
+    throw new Error('actor element not found')
+  }
+
+  const action_element = document.getElementById('action')
+  if (!action_element) {
+    throw new Error('action element not found')
+  }
+
+  const details_element = document.getElementById('details')
+  if (!details_element) {
+    throw new Error('details element not found')
+  }
+
+  return {
+    actor: actor_element,
+    action: action_element,
+    details: details_element,
+  }
+}
